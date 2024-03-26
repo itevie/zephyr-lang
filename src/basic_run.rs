161e16caf3efb57004224bf65b521024f6137274
Isabella <111117650+itevie@users.noreply.github.com>
2024-03-26 10:01:42 +0000

    maybe fix stack problem

--- src/basic_run.rs
+++ src/basic_run.rs
@@ -3,26 +3,32 @@ use std::path::PathBuf;
 use crate::{lexer, parser, runtime::interpreter::Interpreter};
 
 pub fn basic_run(input: String, file_name: String, dir: PathBuf) {
-  let mut interpreter = Interpreter::new(dir.display().to_string());
-
-  let result = match lexer::lexer::lex(input, file_name) {
-    Ok(val) => val,
-    Err(err) => {
-      return crate::die(err.visualise(false));
+  let builder = std::thread::Builder::new()
+    .name("reductor".into())
+    .stack_size(32 * 1024 * 1024);
+  
+  let handler = builder.spawn(move || {let mut interpreter = Interpreter::new(dir.display().to_string());
+    let result = match lexer::lexer::lex(input, file_name) {
+      Ok(val) => val,
+      Err(err) => {
+        return crate::die(err.visualise(false));
+      }
+    };
+  
+    let mut parser = parser::parser::Parser::new(result);
+    let ast = match parser.produce_ast() {
+      Ok(val) => val,
+      Err(err) => {
+        return crate::die(err.visualise(false));
+      }
+    };
+  
+    let value = interpreter.evaluate(parser::nodes::Expression::Program(ast));
+    match value {
+      Err(err) => crate::die(err.visualise(false)),
+      Ok(_) => (),
     }
-  };
+  }).unwrap();
 
-  let mut parser = parser::parser::Parser::new(result);
-  let ast = match parser.produce_ast() {
-    Ok(val) => val,
-    Err(err) => {
-      return crate::die(err.visualise(false));
-    }
-  };
-
-  let value = interpreter.evaluate(parser::nodes::Expression::Program(ast));
-  match value {
-    Err(err) => crate::die(err.visualise(false)),
-    Ok(_) => (),
-  }
+  handler.join().unwrap();
 }
